f173ecbf10989d4187ba48b46e9b96b0c786d404
Batche <61985948+Batche-Hub@users.noreply.github.com>
2021-03-09 18:30:58 +0100

    Update ft_print_comb2.c

--- c00/ex06/ft_print_comb2.c
+++ c00/ex06/ft_print_comb2.c
@@ -1,29 +1,18 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: chbadad <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/02 14:49:56 by chbadad           #+#    #+#             */
-/*   Updated: 2021/02/02 14:58:06 by chbadad          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
-
+// On retrouve putnbr mais en encore plus sophistiqué. ici, on sait qu'il y aura des nombres plus grand que 10, donc si on veut avoir toutes les unités, dizaines centaines..
+// Il faut nécessairement "récupérer" ces dizaines centaines etc.. Vu qu'on est en base 10, on divise par 10. En base 2, on diviserait par deux (et on regarderai le reste d'une division par 2).
 void	ft_putnbr(int nb)
 {
 	if (nb >= 10)
 		ft_putnbr(nb / 10);
 	ft_putchar(nb % 10 + 48);
 }
-
+// Avec ce qu'on appris avant, l'algo est nouvelle fois relativement simple et très similaire à l'exercice précédent.
 void	ft_print_comb2(void)
 {
 	int i;
