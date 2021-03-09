022f25b097a1ce627464a879eb2167a420d50ee8
Batche <61985948+Batche-Hub@users.noreply.github.com>
2021-03-09 18:15:22 +0100

    Update ft_putchar.c

--- c00/ex00/ft_putchar.c
+++ c00/ex00/ft_putchar.c
@@ -1,17 +1,6 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: chbadad <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/02 09:42:40 by chbadad           #+#    #+#             */
-/*   Updated: 2021/02/02 17:17:00 by chbadad          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
-
+// L'idée est d'imprimer un caractère sur la sortie standard. Pour ce faire, on récupère l'adresse du char , on indique la sortie standard, puis le nombre d'octet imprimés
+//(1 en l'occurrence).
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
