5b93fe0e96cceda454639ab97014a20bcf7863ae
Batche <61985948+Batche-Hub@users.noreply.github.com>
2021-03-09 19:33:04 +0100

    Update ft_putstr.c

--- c01/ex05/ft_putstr.c
+++ c01/ex05/ft_putstr.c
@@ -1,17 +1,13 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: chbadad <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/03 10:27:42 by chbadad           #+#    #+#             */
-/*   Updated: 2021/02/03 10:27:56 by chbadad          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
-
+// comme pour putchar, si ce n'est qu'on imprime différents caractère en incrémentant l'adresse du pointeur sur char
+// Il y a de nombreuseus manière de la faire
+// Par exemple
+// int i = 0;
+// while (str[i] != '\0')
+//	write(1, &str[i], 1);
+//	i++;
+// Le '\0' est ce qui clos toute chaine de caractère en C. Ca nous permet donc de savoir tout un tas de choses, par exemple calculer
+// sa longueur, la comparer, faire des opérations sur les valeurs contenues dans la chaîne etc..
 void	ft_putstr(char *str)
 {
 	while (*str)
