c2e000e66be7f7964ae4455ba5186a849328f8e2
Batche <61985948+Batche-Hub@users.noreply.github.com>
2021-03-10 17:23:14 +0100

    Update ft_putstr_non_printable.c

--- c02/ex11/ft_putstr_non_printable.c
+++ c02/ex11/ft_putstr_non_printable.c
@@ -22,7 +22,7 @@ int		is_no_printable(unsigned char c)
 		return (0);
 }
 // La fonction qui sert à mettre en base 16. Ressemble énormément à putnbr. C'est le même principe, si ce n'est qu'ici on sait qu'on ne va pas
-// dépasser 16 en taille. On divise et modulo pou autmatiquement mettre un 0 avant chaque version hexa.
+// dépasser 16 en taille. On divise et modulo pou autmatiquement mettre un 0 avant chaque version hexa (par exemple \0a au lieu de \a).
 void	convert_to_hex(unsigned char c)
 {
 	char *hex;
