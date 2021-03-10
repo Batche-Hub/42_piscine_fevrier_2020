737292f9800f44c7f77f117cf89b38073047d954
Batche <61985948+Batche-Hub@users.noreply.github.com>
2021-03-10 11:09:19 +0100

    Update ft_is_prime.c

--- c05/ex06/ft_is_prime.c
+++ c05/ex06/ft_is_prime.c
@@ -1,15 +1,15 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_is_prime.c                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: chbadad <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/08 10:46:59 by chbadad           #+#    #+#             */
-/*   Updated: 2021/02/08 12:59:18 by chbadad          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
+// Les nombres premiers ne sont JAMAIS pairs, chiffre 2 exclu.
+// Pour savoir si un nombre est premier, on peut tester si les modulos de tous les nombres impairs (un nombre pair avec modulo de 2 renverra toujours 0)
+// avant la racine carrée dudit nombre nous renvoient 0.
+// Si ça renvoie 0, cela veut dire qu'il n'est pas divisible que par lui même et par 1.
+// Evidemment, on ne doit pas tester le nombre lui-même.
+// J'ai mis n < nb / 2 car c'est une limite satisfaisante pour les tests.
+// COmme dit plus haut, on pourrait essayer de calculer une approximation de la racine carrée de nb, mais j'avais la flemme.
+// On pourrait d'ailleurs ne tester que si le nombre est divisible sans reste par les nombres premiers qui le précèdent
+// Mais cela demanderait... de connaître tous les nombres premiers et les tester un par un.
+// Pour tester si votre fonction renvoie le bon résultat : http://compoasso.free.fr/primelistweb/page/prime/liste_online_en.php
+// Ne pas oublier de faire time ./a.out
+// Dépasser les 2 / 3 secondes, c'est RIP.
 int	ft_is_prime(int nb)
 {
 	int n;
