a206243004463f75eaf16dcc17c65ae3f358d0ac
Batche <61985948+Batche-Hub@users.noreply.github.com>
2021-03-10 09:50:07 +0100

    Update ft_strcapitalize.c

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -1,15 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: chbadad <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/04 09:36:32 by chbadad           #+#    #+#             */
-/*   Updated: 2021/02/23 16:46:15 by chbadad          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+//exercice un peu plus complexe où on doit, selon les règle donner dans l'exo, formater une chaîne de char
+// selon les règles.
 
+// 1/ des fonctions qui testent la catégorie des char rencontrés.
+// 2/ la fonction principale qui : met tout en minuscule ce qui est en maj
+// puis selon les tests fait ce qu'elle doit faire c'est à dire : suivre les règles.
+// Si vous êtes piscineux.se je vous conseille de résoudre cet exercice tout.e seul.e, c'est plus rigolo.
+// Faites des tests complets, on peut avoir bon à un moment avec la moulinette, puis ensuite vouloir passer un exo plus dur et avoir faux parce que
+// un autre cas de figure a été testé.
+
+// Notez qu'on pourrait réduire le code de isnotalphanum en testant les fonctions en dessous (et en les mettant au dessus du coup, pour éviter le prototypage)
 int		is_not_alphanum(char c)
 {
 	if (!((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')
