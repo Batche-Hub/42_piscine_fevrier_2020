8af0def652b0fa149782eaedb61377c3ab7951d2
Batche <61985948+Batche-Hub@users.noreply.github.com>
2021-03-10 12:16:45 +0100

    Update ft_print_program_name.c

--- c06/ex00/ft_print_program_name.c
+++ c06/ex00/ft_print_program_name.c
@@ -1,15 +1,18 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_print_program_name.c                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: chbadad <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/09 09:24:49 by chbadad           #+#    #+#             */
-/*   Updated: 2021/02/10 12:21:29 by chbadad          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
+// Le c06 est normalement une formalité sauf si on n'a pas encore vu
+// ce qu'est un argument dans un main.
+// On a deux choses : ac, qui est le nombre d'argument et est AU MOINS égal à 1.
+// Et av, qui est un tableau à deux dimensions, dans lequel sont contenus tous les arguments.
+// ac est toujours au égal à 1 car l'argument qu'on a toujours est tout simplement le nom de notre fichier
+// Pour av, les différents argumentseront fournis par l'intermédiaire des argument qu'on mettre avant l'execution
+// càd :
+// ./a.out mes arguments sont cools
+// Avec l'exemple ci-dessus, on aurait en :
+// av[0] : a.out
+// av[1] : mes
+// av[2] : arguments
+// av[3] : sont
+// av[4] : cools
+// Donc ac == 5
 #include <unistd.h>
 
 void	ft_putstr(char *str)
