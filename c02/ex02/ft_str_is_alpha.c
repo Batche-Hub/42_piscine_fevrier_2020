09ae6dc6f96c44ab390ebe7a3d6f763ba4ba45f2
Batche <61985948+Batche-Hub@users.noreply.github.com>
2021-03-10 09:41:40 +0100

    Update ft_str_is_alpha.c

--- c02/ex02/ft_str_is_alpha.c
+++ c02/ex02/ft_str_is_alpha.c
@@ -1,15 +1,12 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: chbadad <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/03 14:18:03 by chbadad           #+#    #+#             */
-/*   Updated: 2021/02/03 14:25:16 by chbadad          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// man ascii
 
+// Dans cet exercice et les suivant, on doit déterminer si une cdhaîne de caractère
+// est alphabétique, numérique, non imprimable etc.
+// Il faut donc check chaque caractère et vérifier selon les conditions données si tous les caractères
+// sont bien alphabétique numérique ou non imprimable etc.
+// Donc il faut vérifier les valeur ascii de chaque caractère
+// Ici je teste directement avec le pointeur sur char (je teste le caractère pointée à l'adresse donnée)
+// et j'incrémente l'adresse. On pourrait faire également autrement en incrémentant un indice.
 int	ft_str_is_alpha(char *str)
 {
 	while (*str)
