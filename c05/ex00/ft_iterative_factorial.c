5540a7200229d3fe9cdf04a1ad2b3d1ac867ddee
Batche <61985948+Batche-Hub@users.noreply.github.com>
2021-03-10 10:58:40 +0100

    Update ft_iterative_factorial.c

--- c05/ex00/ft_iterative_factorial.c
+++ c05/ex00/ft_iterative_factorial.c
@@ -1,15 +1,4 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: chbadad <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/07 15:20:45 by chbadad           #+#    #+#             */
-/*   Updated: 2021/02/08 10:20:41 by chbadad          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
+// Pas grand chose à dire. Il y a sûrement plein de façon de résoudre cet exo.
 int	ft_iterative_factorial(int nb)
 {
 	int res;
