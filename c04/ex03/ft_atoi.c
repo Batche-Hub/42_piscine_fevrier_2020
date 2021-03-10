ab45d8cdca2b9719f1da59b5992e3e8072efcfb8
Batche <61985948+Batche-Hub@users.noreply.github.com>
2021-03-10 10:54:43 +0100

    Update ft_atoi.c

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -1,15 +1,8 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: chbadad <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/06 13:19:30 by chbadad           #+#    #+#             */
-/*   Updated: 2021/02/07 12:21:48 by chbadad          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
+// A noter : cet implémentation N'EST PAS la même que celle de la fonction disponible sur vos ordi
+// Ceci étant dit, on doit transformer une chaine de char en int, si jamais sont présents
+// des char qui représentent des chiffre à partir d'un moment précis.
+// Si vous êtes en piscine, rappelez-vous : on est dans la table ascii : 0 en chiffre ne correspond pas à 
+// '0' en table ascii.
 int	ft_isspace(char c)
 {
 	if (c == '\t' || c == '\n' || c == '\v'
@@ -25,14 +18,20 @@ int	ft_atoi(char *str)
 
 	res = 0;
 	sign = 1;
+	// tant qu'il y a des char correspond à la définition de isspace (man isspace) on incrémente
 	while (*str && ft_isspace(*str))
 		str++;
+	// tant qu'il y a des + ou - on incrémente. Si on a des moins, à chaque rencontre on multiplie par -1 notre signe
 	while (*str && (*str == '+' || *str == '-'))
 	{
 		if (*str == '-')
 			sign *= -1;
 		str++;
 	}
+	//tant la chaîne existe, et que les char rencontrés sont entre 0 et 9, on fait la formule qui nous permet de passer de la valeur ascii
+	// à la valeur en entier de type int.
+	// Look here => https://www.geeksforgeeks.org/convert-base-decimal-vice-versa/
+	// généralement ce site est un super site.
 	while (*str && *str >= '0' && *str <= '9')
 	{
 		res = res * 10 + *str - 48;
