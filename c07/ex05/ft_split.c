440ce496be23abd037d6ae28a2b4edcf13fd7478
Batche <61985948+Batche-Hub@users.noreply.github.com>
2021-03-10 12:45:01 +0100

    Update ft_split.c

--- c07/ex05/ft_split.c
+++ c07/ex05/ft_split.c
@@ -1,17 +1,6 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: chbadad <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/15 09:42:42 by chbadad           #+#    #+#             */
-/*   Updated: 2021/02/15 09:44:16 by chbadad          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
+//cet exo et l'inverse de str_join. On découpe notre chaine selon un séparateur quelconque pour ranger les résultats dans un tableau de tableau.
 #include <stdlib.h>
-
+// sert à déterminer si un char fait partie de la chaîne separatrice
 int		ft_is_sep(char c, char *sep)
 {
 	while (*sep)
@@ -22,7 +11,7 @@ int		ft_is_sep(char c, char *sep)
 	}
 	return (0);
 }
-
+// sert à compter le nombre de mots qu"on aura. utile pour malloquer notre tableau de tableau
 int		nb_wrds(char *str, char *sep)
 {
 	int count;
@@ -30,8 +19,12 @@ int		nb_wrds(char *str, char *sep)
 	count = 0;
 	while (*str)
 	{
+		// tant que le char fait partie du spéarateur j'incrémente
 		while (*str && ft_is_sep(*str, sep))
 			str++;
+		//si ça ne fait pas partie de la chaîne séparatrice, j'augmente mon nombre de mot de 1, puis tant que ce n'est pas
+		// un séparateur (en bref, tant que je suis dans un mot), j'incrémente ma chaîne, pour ne pas compter autant de mots
+		//qu'il n'y a de lettres.
 		if (!ft_is_sep(*str, sep) && *str)
 		{
 			count++;
@@ -41,7 +34,9 @@ int		nb_wrds(char *str, char *sep)
 	}
 	return (count);
 }
-
+// un strdup pimpé, qui me sert à copier mon mot à l'emplcement désiré dans ma chaîne de chaîne.
+// je récupère directement l'endroit où je suis grâce à l'adresse, je n'ai pas besoin d'indiquer 
+//un indice en plus. Pratique.
 char	*ft_strdup(char *str, char *sep)
 {
 	int		i;
@@ -61,7 +56,9 @@ char	*ft_strdup(char *str, char *sep)
 	word[i] = 0;
 	return (word);
 }
-
+// La fonction principale.
+// On calcule le nombre de mot pour malloc notre tableau de tableau, et ensuite on suit le même cheminement que pour compter les mots.
+// Sauf que au lieu de compter les mots, je copie le mot dans mon tableau de tableau. L'algo est le même.
 char	**ft_split(char *str, char *charset)
 {
 	int		i;
